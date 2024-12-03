c9a15d6148c906f5245ad8bbec7b0b2b63768c7f
ofiel <ofiel@inbox.ru>
2024-12-03 09:28:12 +0300

    6. Правила Checkstyle [#504981 #524229]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,26 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+    String name;
+    public String surname;
+    public static final String STRVALUE = "";
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d) {
+
+    }
+
+    Broken() {
+
+    }
+}
